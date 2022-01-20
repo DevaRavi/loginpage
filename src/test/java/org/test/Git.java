c74bb042dbe105a71da8f2a858b397ace8cebe68
DevaRavi <reltecdeva@gmail.com>
2022-01-20 12:53:41 +0530

    password

--- src/test/java/org/test/Git.java
+++ src/test/java/org/test/Git.java
@@ -4,10 +4,17 @@ package org.test;
 
 
 public class Git {
-	
+	private static void password() {
+		System.out.println("password");
+
+	}
+	public static void main(String[] args) {
+		password();
+	}
+
 	private void Driverpage() {
 	System.out.println(123);
-	
-    
+
+
 	}
 }
